a49893a9605399e178e11e2bfbb4a53ec532b287
Yuga Umemura <yuga0211ume@gmail.com>
2026-01-03 02:03:59 +0900

    手番の処理

--- src/App.tsx
+++ src/App.tsx
@@ -16,12 +16,20 @@ function Square({
 }
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   const handleClick = (i: number) => {
+    if (squares[i]) return;
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "○";
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   };
 
   return (
